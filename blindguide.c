59dd49c6d1585e8290094f2783225080b2e8cad6
Jarno Brils <jarnobrils@gmail.com>
2018-03-07 11:34:48 +0100

    Initial code

---
+++ blindguide.c
@@ -0,0 +1,144 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "blindguide.h"
+
+Coordinate createCoordinate(double x, double y) {
+    Coordinate c;
+    c.x = x;
+    c.y = y;
+    return c;
+}
+
+Borderline createBorderline(Coordinate bottom, Coordinate top, char goodSide) {
+    Borderline bl;
+    bl.bottom = bottom;
+    bl.top = top;
+    bl.length = createVector(top.x - bottom.x, top.y - bottom.y).length;
+    bl.goodSide = goodSide;
+    return bl;
+}
+
+Vector createVector(double x, double y) {
+    Vector v;
+    populateVector(x, y, &v);
+    return v;
+}
+
+void populateVector(double x, double y, Vector * v) {
+    v->length = sqrt(x * x + y * y);
+    v->x = x / v->length;
+    v->y = y / v->length;
+}
+
+void initializeBorders() {
+    int numCoords = sizeof(borderCoordinates) / sizeof(borderCoordinates[0]);
+    numBorderlines = numCoords / 4;
+    borderlines = (struct Borderline *) malloc(numBorderlines * sizeof(struct Borderline));
+    
+    for (int i = 0; i < numBorderlines; i++) {
+        int index = i * 4;
+        borderlines[i] = createBorderline(createCoordinate(borderCoordinates[index], borderCoordinates[index + 1]), createCoordinate(borderCoordinates[index + 2], borderCoordinates[index + 3]), RIGHT);
+    }
+}
+
+double getResistance(double x, double y, double phi, double forceX, double forceY) {
+    #if DEBUG
+        printf("\nGetting resistance for point (%lf,%lf) against direction (%lf, %lf)\n", x, y, forceX, forceY);
+    #endif
+    
+    double resistance = 0;
+    Coordinate point = createCoordinate(x, y);
+    Vector force = createVector(forceX, forceY);
+    Vector toBorder;
+    
+    for (int i = 0; i < numBorderlines; i++) {
+        #if DEBUG
+            printf("\nBorder %d of %d:\n", i+1, numBorderlines);
+        #endif
+        double approaching = approachingBorder(&point, &borderlines[i], &force, &toBorder);
+        if (approaching == RESIST) {
+            resistance = fmax(resistance, getBorderResistance(&force, &toBorder));
+        } else if (approaching == STOP) {
+            return 1.0;
+        }
+    }
+    
+    return resistance;
+}
+
+double getAcceleration(Vector * force) {
+    return force->length / MASS;
+}
+
+char approachingBorder(Coordinate * p, Borderline * b, Vector * force, Vector * toBorder) {
+    Coordinate * v = &(b->bottom);
+    Coordinate * w = &(b->top);
+    
+    double l2 = b->length * b->length;
+    double t = fmax(0.0, fmin(1.0, ((p->x - v->x) * (w->x - v->x) + (p->y - v->y) * (w->y - v->y)) / l2));
+    double x = v->x + t * (w->x - v->x);
+    double y = v->y + t * (w->y - v->y);
+    
+    populateVector(x - p->x, y - p->y, toBorder);
+    toBorder->length -= RADIUS;
+    
+    double d = (p->x - v->x) * (w->y - v->y) - (p->y - v->y) * (w->x - v->x);
+    
+    double dot = (force->x * toBorder->x) + (force->y * toBorder->y);
+    char goingToBorder = dot > 0 ? 1 : 0;
+    
+    #if DEBUG
+        printf("vx: %lf, vy: %lf, wx: %lf, wy: %lf\n", v->x, v->y, w->x, w->y);
+        printf("l2: %lf, t: %lf, x: %lf, y: %lf\n", l2, t, x, y);
+        printf("To border: distance: %lf, x: %lf, y: %lf\n", toBorder->length, toBorder->x, toBorder->y);
+        printf("Force: distance: %lf, x: %lf, y: %lf\n", force->length, force->x, force->y);
+        printf("Good side: %s, Side: %s\n", b->goodSide == LEFT ? "left" : "right", d < 0 ? "left" : "right");
+        printf("Dot: %lf, Going to border: %d\n", dot, goingToBorder);
+    #endif
+    
+    if ((b->goodSide == LEFT && d < 0) || (b->goodSide == RIGHT && d > 0)) {
+        // p is on the good side of the border
+        if (goingToBorder) {
+            return RESIST;
+        }            
+    } else {
+        // p is on the bad side of the border
+        if (!goingToBorder) {
+            return STOP;
+        }
+    }
+    
+    return NOTHING;
+}
+
+double getBorderResistance(Vector * force, Vector * toBorder) {
+    if (toBorder->length <= 0) {
+        return 1.0;
+    }
+    
+    double a = getAcceleration(force);
+    double t = sqrt((2 * toBorder->length) / a);
+    double resistance = (RESISTANCE_TIME - STOP_TIME) / (RESISTANCE_TIME - t);
+    #if DEBUG
+        printf("a: %lf, t: %lf, res: %lf\n", a, t, resistance);
+    #endif
+    
+    return fmin(fmax(0.0, resistance), 1.0);
+}
+
+void cleanup() {
+    free(borderlines);
+}
+
+/*
+ * This can be used for standalone testing
+ */
+int main() {
+    printf("Initializing borders...\n");
+    initializeBorders();
+    printf("\nResistance: %lf\n", getResistance(-3.2, -11.0, 0.0, 0.7, 0.5));
+    printf("Cleaning up...\n");
+    cleanup();
+}
---
+++ blindguide.h
@@ -0,0 +1,128 @@
+// Do we need debug output?
+#define DEBUG 1
+
+// Mass of the robot in kg
+#define MASS 30
+// Radius around the center of the robot that must stay between the borders in meter
+#define RADIUS 0.8
+// When the robot reaches the border within this many seconds, stop immediately
+#define STOP_TIME 0.5
+// When the robot reaches the border within this many seconds, start resisting
+#define RESISTANCE_TIME 3.5
+
+// THESE DO NOT NEED TO BE CHANGED
+#define LEFT -1
+#define RIGHT 1
+#define NOTHING 0
+#define RESIST 1
+#define STOP 2
+
+// The coordinates for the initial borders (bottom_x, bottom_y, top_x, top_y)
+// Note that the border is assumed to be safe to the right of this line
+// So define the top and bottom accordingly
+double borderCoordinates[8] = {
+    -1.5, -9.0,
+    -1.5, 9.0,
+    1.5, 9.0,
+    1.5, -9.0
+};
+
+// A 2D coordinate structure
+typedef struct Coordinate {
+    double x;
+    double y;
+} Coordinate;
+
+// Definition of a border line structure
+// bottom: Coordinate
+// top: Coordinate
+// length: length of border line
+// goodSide: side which is considered 'safe' (LEFT or RIGHT)
+typedef struct Borderline {
+    struct Coordinate bottom;
+    struct Coordinate top;
+    double length;
+    char goodSide; 
+} Borderline;
+
+// A 2D vector structure with additional length value
+// Use as unit vector recommended (hence the length)
+// Unit vector can be constructed using createVector()
+typedef struct Vector {
+    double x;
+    double y;
+    double length;
+} Vector;
+
+// The number of border lines
+int numBorderlines;
+// Pointer to an array of border line structures
+struct Borderline * borderlines;
+
+/*
+ * Creates and returns a Coordinate structure with the given x and y coordinates.
+ */
+Coordinate createCoordinate(double x, double y);
+
+/*
+ * Creates and returns a Borderline structure with the given bottom and top Coordinates
+ * and the given good side.
+ * Also computes and stores the length of this border line.
+ */
+Borderline createBorderline(Coordinate bottom, Coordinate top, char goodSide);
+
+/*
+ * Creates and returns a Vector structure for the given x and y values.
+ * Calls createVector().
+ */
+Vector createVector(double x, double y);
+
+/*
+ * Fills the given Vector structure with the given x and y coordinates.
+ * Will convert the vector to a unit vector.
+ * Computes and stores the length of the original vector.
+ */
+void populateVector(double x, double y, Vector * v);
+
+/*
+ * Creates and fills the borderlines array based on the values in borderCoordinates.
+ */
+void initializeBorders();
+
+/*
+ * Compute the necessary resistance when the robot is at the position defined by x, y and phi
+ * and the robot is pushed according to forceX and forceY, where forceX and forceY constitute the
+ * force vector.
+ * Note that the units of x and y are assumed to be meters, and forceX and forceY Newtons.
+ */
+double getResistance(double x, double y, double phi, double forceX, double forceY);
+
+/*
+ * Calculate the acceleration along the given force vector.
+ * Uses the defined MASS of the robot.
+ * Formula: a = F / m.
+ */
+double getAcceleration(Vector * force);
+
+/*
+ * Determines whether the robot at position p with the predefined RADIUS is approaching border line b
+ * when it is pushed along force vector force.
+ * toBorder will be populated by the vector from p to the closest point along the border line.
+ * If the robot is on the 'good' side of the border, and the robot is pushed towards the border, returns RESIST.
+ * If the robot is on the 'bad' side of the border, and the robot is pushed away from the border, returns STOP.
+ * Else, returns 0.
+ */
+char approachingBorder(Coordinate * p, Borderline * b, Vector * force, Vector * toBorder);
+
+/*
+ * Determines the resistance needed when the robot is pushed with the given force along the toBorder vector.
+ * Uses RESISTANCE_TIME and STOP_TIME to create a linearly increasing resistance when the time to traverse
+ * the toBorder vector decreases.
+ * Return value is clamped between 0 and 1.
+ */
+double getBorderResistance(Vector * force, Vector * toBorder);
+
+/*
+ * Frees the memory allocated for the borderlines array.
+ */
+void cleanup();
