316ac9a8b0d04d7af954c53f625b5cfd9b579aff
Jarno Brils <jarnobrils@gmail.com>
2018-03-13 16:47:26 +0100

    Changed files for first practical test.

--- blindguide.c
+++ blindguide.c
@@ -1,5 +1,9 @@
+
+#define S_FUNCTION_NAME blindguide
+#define S_FUNCTION_LEVEL 2
+
 /*
- * Copyright 2018 Anne Kolmans, Dylan ter Veen, Jarno Brils, Renée van Hijfte, and Thomas Wiepking (TU/e Project Robots Everywhere 2017/2018 Q3 Group 12)
+ * Copyright 2018 Anne Kolmans, Dylan ter Veen, Jarno Brils, Ren??e van Hijfte, and Thomas Wiepking (TU/e Project Robots Everywhere 2017/2018 Q3 Group 12)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +18,16 @@
  * limitations under the License.
  */
 
+#include "simstruc.h"
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "blindguide.h" 
 
-#include "blindguide.h"
+/* include h-files */
+#include "Simulink/Bus/busses/bus.h"
+#include "Global_par/constants.h"
+#include "GeneralFunctions/generic_functions.h"
 
 Coordinate createCoordinate(double x, double y) {
     Coordinate c;
@@ -42,149 +51,129 @@ Vector createVector(double x, double y) {
     return v;
 }
 
-void populateVector(double x, double y, Vector * v) {
-    v->length = sqrt(x * x + y * y);
-    v->x = x / v->length;
-    v->y = y / v->length;
-}
+/***************************
+ * Input Ports definitions *
+ ***************************/
+#define NINPUTS     2                   /* Number of input ports (0...)*/
+#define NINPUTS0    3                   /* cur x y o */
+#define NINPUTS1    3                   /* Jerrel's Forces */
+double NINPUTS_BGuide[NINPUTS] =  {NINPUTS0,NINPUTS1};
 
-void createBorderlineArray(BorderlineArray * ba, size_t size) {
-    ba->borderlines = (struct Borderline *) malloc(size * sizeof(struct Borderline));
-    ba->size = 0;
-    ba->capacity = size;
-}
 
-void addToBorderlineArray(BorderlineArray * ba, Borderline element) {
-    if (ba->size >= ba->capacity) {
-        ba->capacity *= 2;
-        ba->borderlines = (struct Borderline *) realloc(ba->borderlines, ba->capacity * sizeof(struct Borderline));
-    }
-    ba->borderlines[ba->size++] = element;
-}
+/****************************
+ * Output Ports definitions *
+ ****************************/
+#define NOUTPUTS    1
+#define NOUTPUTS0   1              /* Resistance */
 
-void freeBorderlineArray(BorderlineArray * ba) {
-    free(ba->borderlines);
-    ba->borderlines = NULL;
-    ba->size = ba->capacity = 0;
-}
-
-void initializeBorders() {
-    size_t numCoords = sizeof(borderCoordinates) / sizeof(borderCoordinates[0]);
-    size_t numBorderlines = numCoords / 4;
-    createBorderlineArray(&borderlines, numBorderlines);
+/*************************************************************************/
+static void mdlInitializeSizes(SimStruct *S)
+{
+    int i,n1, n2, Rworksize;
     
-    for (int i = 0; i < numBorderlines; i++) {
-        int index = i * 4;
-        addBorder(borderCoordinates[index], borderCoordinates[index + 1], borderCoordinates[index + 2], borderCoordinates[index + 3], RIGHT);
-    }
-}
-
-void addBorder(double bottomX, double bottomY, double topX, double topY, enum side goodSide) {
-    if (borderlines.borderlines == NULL || borderlines.capacity <= 0) {
-        createBorderlineArray(&borderlines, 1);
-    }
-    struct Coordinate bottom = createCoordinate(bottomX, bottomY);
-    struct Coordinate top = createCoordinate(topX, topY);
-    struct Borderline bl = createBorderline(bottom, top, goodSide);
-    addToBorderlineArray(&borderlines, bl);
-}
-
-double getResistance(double x, double y, double phi, double forceX, double forceY) {
-    #if DEBUG
-        printf("\nGetting resistance for point (%lf,%lf) against direction (%lf, %lf)\n", x, y, forceX, forceY);
-    #endif
-    
-    double resistance = 0;
-    Coordinate point = createCoordinate(x, y);
-    Vector force = createVector(forceX, forceY);
-    Vector toBorder;
-    
-    for (int i = 0; i < borderlines.size; i++) {
-        #if DEBUG
-            printf("\nBorder %d of %d:\n", i+1, borderlines.size);
-        #endif
-        enum action a = approachingBorder(&point, &(borderlines.borderlines[i]), &force, &toBorder);
-        if (a == RESIST) {
-            resistance = fmax(resistance, getBorderResistance(&force, &toBorder));
-        } else if (a == STOP) {
-            return 1.0;
-        }
+    ssSetNumSFcnParams(S, 0);
+    if (ssGetNumSFcnParams(S) != ssGetSFcnParamsCount(S)) {
+        return;
     }
     
-    return resistance;
-}
-
-double getAcceleration(Vector * force) {
-    return force->length / MASS;
-}
-
-enum action approachingBorder(Coordinate * p, Borderline * b, Vector * force, Vector * toBorder) {
-    Coordinate * v = &(b->bottom);
-    Coordinate * w = &(b->top);
-    
-    double l2 = b->length * b->length;
-    double t = fmax(0.0, fmin(1.0, ((p->x - v->x) * (w->x - v->x) + (p->y - v->y) * (w->y - v->y)) / l2));
-    double x = v->x + t * (w->x - v->x);
-    double y = v->y + t * (w->y - v->y);
-    
-    populateVector(x - p->x, y - p->y, toBorder);
-    toBorder->length -= RADIUS;
-    
-    double d = (p->x - v->x) * (w->y - v->y) - (p->y - v->y) * (w->x - v->x);
-    
-    double dot = (force->x * toBorder->x) + (force->y * toBorder->y);
-    char goingToBorder = dot > 0 ? 1 : 0;
-    
-    #if DEBUG
-        printf("vx: %lf, vy: %lf, wx: %lf, wy: %lf\n", v->x, v->y, w->x, w->y);
-        printf("l2: %lf, t: %lf, x: %lf, y: %lf\n", l2, t, x, y);
-        printf("To border: distance: %lf, x: %lf, y: %lf\n", toBorder->length, toBorder->x, toBorder->y);
-        printf("Force: distance: %lf, x: %lf, y: %lf\n", force->length, force->x, force->y);
-        printf("Good side: %s, Side: %s\n", b->goodSide == LEFT ? "left" : "right", d < 0 ? "left" : "right");
-        printf("Dot: %lf, Going to border: %d\n", dot, goingToBorder);
-    #endif
-    
-    if ((b->goodSide == LEFT && d < 0) || (b->goodSide == RIGHT && d > 0)) {
-        // p is on the good side of the border
-        if (goingToBorder) {
-            return RESIST;
-        }            
-    } else {
-        // p is on the bad side of the border
-        if (!goingToBorder) {
-            return STOP;
-        }
+    /***************************
+     * Input Ports definitions *
+     ***************************/
+    if (!ssSetNumInputPorts(S,NINPUTS)) return;
+    for(i=0;i<NINPUTS;i++)
+    {
+        /* input port i */
+        ssSetInputPortWidth(S,i,NINPUTS_BGuide[i]);
+        ssSetInputPortDirectFeedThrough(S,i,1);
+        ssSetInputPortRequiredContiguous(S,i,1);
+        ssSetInputPortDataType(S,i,SS_DOUBLE);
     }
     
-    return NOTHING;
-}
-
-double getBorderResistance(Vector * force, Vector * toBorder) {
-    if (toBorder->length <= 0) {
-        return 1.0;
-    }
+    /****************************
+     * Output Ports definitions *
+     ****************************/
+    if (!ssSetNumOutputPorts(S, NOUTPUTS)) return;
+    ssSetOutputPortWidth(S,0,NOUTPUTS0);
+    ssSetOutputPortDataType(S,0,SS_DOUBLE);
     
-    double a = getAcceleration(force);
-    double t = sqrt((2 * toBorder->length) / a);
-    double resistance = (RESISTANCE_TIME - t) / (RESISTANCE_TIME - STOP_TIME);
-    #if DEBUG
-        printf("a: %lf, t: %lf, res: %lf\n", a, t, resistance);
-    #endif
+
+    /***********************
+     * Definition of States *
+     ************************/
+    ssSetNumContStates(S, 0);
+    ssSetNumDiscStates(S, 0);
     
-    return fmin(fmax(0.0, resistance), 1.0);
+    /**********************
+     * Default Definitions *
+     **********************/
+    ssSetNumSampleTimes(S, 1);
+
+    ssSetNumRWork(S, 0);
+    ssSetNumIWork(S, 0);
+    ssSetNumPWork(S, 0);
+    ssSetNumModes(S, 0);
+}
+    
+/*********************************************************************************************/
+#if defined(MATLAB_MEX_FILE)
+#define MDL_SET_INPUT_PORT_DIMENSION_INFO
+static void mdlSetInputPortDimensionInfo(SimStruct        *S,
+int_T            port,
+const DimsInfo_T *dimsInfo)
+{
+    if(!ssSetInputPortDimensionInfo(S, port, dimsInfo)) return;
+}
+#endif
+
+#define MDL_SET_OUTPUT_PORT_DIMENSION_INFO
+#if defined(MDL_SET_OUTPUT_PORT_DIMENSION_INFO)
+static void mdlSetOutputPortDimensionInfo(SimStruct        *S,
+int_T            port,
+const DimsInfo_T *dimsInfo)
+{
+    if (!ssSetOutputPortDimensionInfo(S, port, dimsInfo)) return;
 }
 
-void cleanup() {
-    freeBorderlineArray(&borderlines);
+#endif
+
+/*************************************************************************/
+static void mdlInitializeSampleTimes(SimStruct *S)
+{
+    ssSetSampleTime(S, 0, INHERITED_SAMPLE_TIME);
+    ssSetOffsetTime(S, 0, FIXED_IN_MINOR_STEP_OFFSET);
 }
 
-/*
- * This can be used for standalone testing
- */
-int main() {
-    printf("Initializing borders...\n");
+/*************************************************************************/
+#define MDL_INITIALIZE_CONDITIONS
+static void mdlInitializeConditions(SimStruct *S)
+{
+#ifndef MATLAB_MEX_FILE
+#endif
+}
+
+/*************************************************************************/
+static void mdlOutputs(SimStruct *S, int_T tid)
+{
+    /* Input Ports */
+    double* cur_xyo = (double*)ssGetInputPortRealSignalPtrs(S,0);             /* motionbus */
+    double* Fvec    = (double*)ssGetInputPortRealSignalPtrs(S,1);
+
+    /* Output Ports */
+    double* resistance      = (double*)ssGetOutputPortSignal(S,0);
+   
     initializeBorders();
-    printf("\nResistance: %lf\n", getResistance(0.6, 0.0, 0.0, 0.7, 0.5));
-    printf("Cleaning up...\n");
+    *resistance = getResistance(cur_xyo[0], cur_xyo[1], cur_xyo[2], Fvec[0], Fvec[1]);
     cleanup();
+    
 }
+/*************************************************************************/
+static void mdlTerminate(SimStruct *S)
+{
+
+}
+
+#ifdef  MATLAB_MEX_FILE    /* Is this file being compiled as a MEX-file? */
+#include "simulink.c"      /* MEX-file interface mechanism */
+#else
+#include "cg_sfun.h"       /* Code generation registration function */
+#endif
--- blindguide.h
+++ blindguide.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Anne Kolmans, Dylan ter Veen, Jarno Brils, Renée van Hijfte, and Thomas Wiepking (TU/e Project Robots Everywhere 2017/2018 Q3 Group 12)
+ * Copyright 2018 Anne Kolmans, Dylan ter Veen, Jarno Brils, Ren??e van Hijfte, and Thomas Wiepking (TU/e Project Robots Everywhere 2017/2018 Q3 Group 12)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@
 // Mass of the robot in kg
 #define MASS 30
 // Radius around the center of the robot that must stay between the borders in meter
-#define RADIUS 0.8
+#define RADIUS 0.3
 // When the robot reaches the border within this many seconds, stop immediately
 #define STOP_TIME 0.5
 // When the robot reaches the border within this many seconds, start resisting
@@ -177,3 +177,143 @@ double getBorderResistance(Vector * force, Vector * toBorder);
  * Frees the memory allocated for the borderlines array.
  */
 void cleanup();
+
+
+void populateVector(double x, double y, Vector * v) {
+    v->length = sqrt(x * x + y * y);
+    v->x = x / v->length;
+    v->y = y / v->length;
+}
+
+void createBorderlineArray(BorderlineArray * ba, size_t size) {
+    ba->borderlines = (struct Borderline *) malloc(size * sizeof(struct Borderline));
+    ba->size = 0;
+    ba->capacity = size;
+}
+
+void addToBorderlineArray(BorderlineArray * ba, Borderline element) {
+    if (ba->size >= ba->capacity) {
+        ba->capacity *= 2;
+        ba->borderlines = (struct Borderline *) realloc(ba->borderlines, ba->capacity * sizeof(struct Borderline));
+    }
+    ba->borderlines[ba->size++] = element;
+}
+
+void freeBorderlineArray(BorderlineArray * ba) {
+    free(ba->borderlines);
+    ba->borderlines = NULL;
+    ba->size = ba->capacity = 0;
+}
+
+void initializeBorders() {
+    size_t numCoords = sizeof(borderCoordinates) / sizeof(borderCoordinates[0]);
+    size_t numBorderlines = numCoords / 4;
+    createBorderlineArray(&borderlines, numBorderlines);
+    unsigned int i = 0;
+    for (i = 0; i < numBorderlines; i++) {
+        int index = i * 4;
+        addBorder(borderCoordinates[index], borderCoordinates[index + 1], borderCoordinates[index + 2], borderCoordinates[index + 3], RIGHT);
+    }
+}
+
+void addBorder(double bottomX, double bottomY, double topX, double topY, enum side goodSide) {
+    if (borderlines.borderlines == NULL || borderlines.capacity <= 0) {
+        createBorderlineArray(&borderlines, 1);
+    }
+    struct Coordinate bottom = createCoordinate(bottomX, bottomY);
+    struct Coordinate top = createCoordinate(topX, topY);
+    struct Borderline bl = createBorderline(bottom, top, goodSide);
+    addToBorderlineArray(&borderlines, bl);
+}
+
+double getResistance(double x, double y, double phi, double forceX, double forceY) {
+    #if DEBUG
+        printf("\nGetting resistance for point (%lf,%lf) against direction (%lf, %lf)\n", x, y, forceX, forceY);
+    #endif
+    
+    double resistance = 0;
+    Coordinate point = createCoordinate(x, y);
+    Vector force = createVector(forceX, forceY);
+    Vector toBorder;
+    unsigned int i = 0;
+    for (i = 0; i < borderlines.size; i++) {
+        #if DEBUG
+    //        printf("\nBorder %d of %d:\n", i+1, borderlines.size);
+        #endif
+        enum action a = approachingBorder(&point, &(borderlines.borderlines[i]), &force, &toBorder);
+        if (a == RESIST) {
+            resistance = fmax(resistance, getBorderResistance(&force, &toBorder));
+        } else if (a == STOP) {
+            return 1.0;
+        }
+    }
+    
+    return resistance;
+}
+
+double getAcceleration(Vector * force) {
+    return force->length / MASS;
+}
+
+enum action approachingBorder(Coordinate * p, Borderline * b, Vector * force, Vector * toBorder) {
+    Coordinate * v = &(b->bottom);
+    Coordinate * w = &(b->top);
+    
+    double l2 = b->length * b->length;
+    double t = fmax(0.0, fmin(1.0, ((p->x - v->x) * (w->x - v->x) + (p->y - v->y) * (w->y - v->y)) / l2));
+    double x = v->x + t * (w->x - v->x);
+    double y = v->y + t * (w->y - v->y);
+    
+    populateVector(x - p->x, y - p->y, toBorder);
+    toBorder->length -= RADIUS;
+    
+    double d = (p->x - v->x) * (w->y - v->y) - (p->y - v->y) * (w->x - v->x);
+    
+    double dot = (force->x * toBorder->x) + (force->y * toBorder->y);
+    char goingToBorder = dot > 0 ? 1 : 0;
+    
+    #if DEBUG
+        printf("vx: %lf, vy: %lf, wx: %lf, wy: %lf\n", v->x, v->y, w->x, w->y);
+        printf("l2: %lf, t: %lf, x: %lf, y: %lf\n", l2, t, x, y);
+        printf("To border: distance: %lf, x: %lf, y: %lf\n", toBorder->length, toBorder->x, toBorder->y);
+        printf("Force: distance: %lf, x: %lf, y: %lf\n", force->length, force->x, force->y);
+        printf("Good side: %s, Side: %s\n", b->goodSide == LEFT ? "left" : "right", d < 0 ? "left" : "right");
+        printf("Dot: %lf, Going to border: %d\n", dot, goingToBorder);
+    #endif
+    
+    if ((b->goodSide == LEFT && d < 0) || (b->goodSide == RIGHT && d > 0)) {
+        // p is on the good side of the border
+        if (goingToBorder) {
+            return RESIST;
+        }            
+    } else {
+        // p is on the bad side of the border
+        if (!goingToBorder) {
+            return STOP;
+        }
+    }
+    
+    return NOTHING;
+}
+
+double getBorderResistance(Vector * force, Vector * toBorder) {
+    if (toBorder->length <= 0) {
+        return 1.0;
+    }
+    
+    double a = getAcceleration(force);
+    double t = sqrt((2 * toBorder->length) / a);
+    double resistance = (RESISTANCE_TIME - t) / (RESISTANCE_TIME - STOP_TIME);
+    #if DEBUG
+        printf("a: %lf, t: %lf, res: %lf\n", a, t, resistance);
+    #endif
+        
+    double x = (1 - toBorder->length) / (1 - 0.0);
+    return fmin(fmax(0.0, x), 1.0) / 2;
+    
+    //return fmin(fmax(0.0, resistance), 1.0);
+}
+
+void cleanup() {
+    freeBorderlineArray(&borderlines);
+}
