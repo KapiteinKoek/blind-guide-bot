b50332aa8af6b6a6a97e800cca385dcbb8147284
Jarno Brils <jarnobrils@gmail.com>
2018-03-24 17:33:27 +0100

    A bunch of test cases (mostly commented out).

--- tester.c
+++ tester.c
@@ -29,10 +29,21 @@ int main() {
     printf("Initializing borders...\n");
     initializeBorders();
     double y = 0;
-    double x;
-    for (x = 1.5; x > -2; x -= 0.01) {
-        printf("x: %lf, y: %lf\tresistance: %lf\n", x, y, getResistance(x, y, 0.0, -21.2, 15.0));
+    double x = -4;
+    double phi = 0;
+    double ox = -2;
+    double oy = 0.6;
+    for (ox = -2; ox <= 2; ox += 0.2) {
+        //printf("%lf\n", phi);
+        double obstacles[4] = {ox, oy, 0, 1.5};
+        //printf("%lf\n", getResistance(0, 0, 0.5 * PI, 0, 20, 2, obstacles));
     }
-    printf("Cleaning up...\n");
+    //for (x = 1.5; x > -2; x -= 0.01) {
+        //printf("\nx: %lf, y: %lf\tresistance: %lf\n", x, y, getResistance(x, y, 0.0, -21.2, 15.0));
+    //}
+    double obstacles[2] = {1, -2};
+        printf("%lf\n", getResistance(0, 0, 2.5 * PI, 10, -7, 1, obstacles));
+        printf("%lf\n", getResistance(0, 0, 2.75 * PI, -10, -7, 1, obstacles));
+    printf("\nCleaning up...\n");
     cleanup();
 }
